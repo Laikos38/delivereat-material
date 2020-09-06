ea976211fc6b77c172fa4af0d80284c80ed0eb89
Francisco <j.f.maurino@gmail.com>
2020-09-06 17:03:36 -0300

    Agrego items a barra lateral a modo de placeholders.

--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import swal from 'sweetalert';
 
 @Component({
   selector: 'app-topbar',
@@ -12,4 +13,11 @@ export class TopbarComponent implements OnInit {
   ngOnInit() {
   }
 
+  openDialog() {
+    swal({
+      title: "Acerca de DeliverEat",
+      text: "Hecho con mucho amor y principalmente paciencia por el grupo 9 de ISW 4k2 2020",
+      icon: "info",
+    });
+  }
 }
